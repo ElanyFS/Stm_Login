a3c922ae811db3a2d5f534fe9155ba5a88ad2e40
Elany Souza <ellanysouza08@gmail.com>
2023-09-25 08:44:42 -0300

    query

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -4,16 +4,72 @@ namespace app\controllers;
 
 use League\Plates\Engine;
 
+use function config\connection;
+
 class Controller
 {
     public static function view($view, $data = [])
     {
-        $viewPath = dirname(__FILE__, 2 ) . "/view";
+        $viewPath = dirname(__FILE__, 2) . "/view";
 
         // Create new Plates instance
         $templates = new Engine($viewPath);
 
         // Render a template
-        echo $templates->render($view, ['name' => 'Jonathan']);
+        echo $templates->render($view, $data);
+    }
+
+
+    public static function select($table, $field = '*')
+    {
+        $connect = connection();
+
+        //select * from table;
+        $query = "select {$field} from {$table}";
+
+        $prepare = $connect->query($query);
+
+        return $prepare->fetchAll();
+    }
+
+    public static function create($table, $value)
+    {
+        $connect = connection();
+
+        // insert into table (column) value (:column);
+
+        $query = "insert into {$table} (";
+        $query .= implode(", ", array_keys($value)) . ") value (";
+        $query .=  ":" . implode(' , :', array_keys($value)) . ")";
+
+        $prepare = $connect->prepare($query);
+
+        return $prepare->execute($value);
+    }
+
+    public static function update()
+    {
+    }
+
+    public static function delete()
+    {
+    }
+
+    public static function find($table, $field = '*', $key, $value)
+    {
+        // select * from table where id = id
+
+        $connect = connection();
+
+        $query = "select {$field} from {$table} where {$key} = :{$key}";
+
+        $prepare = $connect->prepare($query);
+
+        $prepare->execute([
+            $key => $value
+        ]);
+
+        return $prepare->fetch();
+        // var_dump($prepare->fetch());
     }
 }
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,9 +2,15 @@
 
 namespace app\controllers;
 
-class Home{
-    public function index(){
-        Controller::view('Login');
+class Home
+{
+    public function index()
+    {
+
+        // Controller::create('usuarios', ['name' => 'Maria Eduarda', 'email' => 'duda@gmail.com', 'password' => '123456']);
+
+        $dataFromTable = Controller::select('usuarios');
+
+        Controller::view('Login', ['data' => $dataFromTable]);
     }
 }
-
